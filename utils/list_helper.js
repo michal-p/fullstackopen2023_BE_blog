5c4994ed6fc4c59bb75c22e0d74cf0ca22c3acf3
Michal Puchy <puchy.michal@gmail.com>
2024-01-31 16:51:20 +0100

    4.7*: helper functions and unit tests, step5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,10 +1,8 @@
 // Load the full build.
 var _ = require('lodash')
 
-const dummy = (blogs) => {
-  console.log(blogs)
-  return 1
-}
+// eslint-disable-next-line no-unused-vars
+const dummy = (blogs) => 1
 
 const totalLikes = (blogs) => {
   return blogs.reduce((acc, curr) => {
@@ -17,28 +15,48 @@ const favoriteBlog = (blogs) => {
 
   return blogs.reduce((acc, curr) => {
     const { title, author, likes } = curr
-    return !acc.likes || likes >= acc.likes
+    return likes >= (acc.likes || 0)
       ? { title, author, likes }
       : acc
   }, {})
 }
 
-const mostBlogs = (blogsList) => {
-  if (blogsList.length === 0) return { author: null, blogs: 0 }
+const mostBlogs = (blogList) => {
+  if (blogList.length === 0) return null
 
-  const biggestWriters = _.countBy(blogsList, 'author' )
-  const biggestValue = _.max(Object.values(biggestWriters))
+  const biggestWriters = _.countBy(blogList, 'author' )
+  const biggestValue = _.max(_.values(biggestWriters))
   const biggestAuthor = _.findKey(biggestWriters, (value) => value === biggestValue)
 
-  return {
-    author: biggestAuthor,
-    blogs: biggestValue
-  }
+  return { author: biggestAuthor, blogs: biggestValue }
+}
+
+const mostLikes = (blogList) => {
+  if (blogList.length === 0) return null
+
+  const authorsWithMaxLikes = _.reduce(blogList, (acc, curr) => {
+    if (curr.author && curr.likes) {
+      if (acc[curr.author]) {
+        acc[curr.author] += curr.likes
+      } else {
+        acc[curr.author] = curr.likes
+      }
+    }
+    return acc
+  }, {})
+
+  return _.transform(authorsWithMaxLikes, (result, likes, author) => {
+    if (likes > (result.likes || 0)) {
+      result.author = author
+      result.likes = likes
+    }
+  }, {})
 }
 
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
-  authorWithTheMostBlogs: mostBlogs
+  mostBlogs,
+  mostLikes
 }
