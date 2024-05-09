682da40d85aca71fb7f15f1f3169b42b7da1a6cd
Michal Puchy <puchy.michal@gmail.com>
2024-05-09 17:10:19 +0200

    4.18: Blog List Expansion, step 6

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ require('express-async-errors')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const cors = require('cors')
+
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+
 const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 
@@ -23,6 +26,7 @@ app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
 
+app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 
