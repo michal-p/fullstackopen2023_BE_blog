b8b9fd97179fe7e471a95098312ac451a970109a
Michal Puchy <puchy.michal@gmail.com>
2024-05-11 23:09:45 +0200

    4.21*: Blog List Expansion, step 9

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -47,7 +47,27 @@ blogsRouter.post('/', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndDelete(request.params.id)
+  const blog = await Blog.findById(request.params.id)
+  if (!blog) {
+    return response.status(404).json({ error: 'blog has not been found' })
+  }
+
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id)
+  if (!user) {
+    return response.status(404).end()
+  }
+
+
+  if (blog.user.toString() !== user._id.toString()) {
+    return response.status(403).json({ error: 'blog does not belong to user' })
+  }
+
+  await Blog.deleteOne(blog)
   response.status(204).end()
 })
 
