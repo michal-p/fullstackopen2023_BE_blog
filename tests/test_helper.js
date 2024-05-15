74abe3490c47730040124c0b4adbf17790362597
Michal Puchy <puchy.michal@gmail.com>
2024-05-15 16:24:38 +0200

    4.23*: Blog List Expansion, step 11 -> Refactor tests and enhance blog creation with user authentication; update helper for dynamic initial blogs; improve blog API with user association and validation; streamline test script naming in package.json; adjust app middleware setup for blog routes; fix blog update request format.

--- tests/test_helper.js
+++ tests/test_helper.js
@@ -1,28 +1,34 @@
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
-const initialBlogs = [
-  {
-    _id: '5a422a851b54a676234d17f7',
-    title: 'test React patterns',
-    author: 'Michael Chan',
-    url: 'https://reactpatterns.com/',
-    likes: 7,
-    __v: 0
-  },
-  {
-    _id: '5a422aa71b54a676234d17f8',
-    title: 'Go To Statement Considered Harmful',
-    author: 'Edsger W. Dijkstra',
-    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-    likes: 5,
-    __v: 0
-  }
-]
+const initialBlogs = (user) => {
+  return (
+    [
+      {
+        _id: '5a422a851b54a676234d17f7',
+        title: 'test React patterns',
+        author: 'Michael Chan',
+        url: 'https://reactpatterns.com/',
+        likes: 7,
+        user: user.id,
+        __v: 0
+      },
+      {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        user: user.id,
+        __v: 0
+      }
+    ]
+  )
+}
 
 const blogsInDb = async () => {
-  const blogs = await Blog.find({})
-  return blogs.map(blog => blog.toJSON())
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+  return blogs.map(blog => blog.toJSON()) //!This method toJSON() in context of Mongoose deserialize documents from MongoDB to Javascript objects
 }
 
 const nonExistingBlogId = async () => {
@@ -43,7 +49,7 @@ const rootUserInDb = async () => {
 
   if (!rootUser) throw new Error('Root user not found in the database')
 
-  return rootUser.toJSON()
+  return rootUser
 }
 
 module.exports = {
